09a2f8aef0890bee9f7677989e22f2847576ce83
Harshal Pawar <harshalpawar992@gmail.com>
2026-07-10 15:36:12 +0530

    Add files via upload

---
+++ C programming/assignment3/assig3.4.c
@@ -0,0 +1,25 @@
+//Check the given number is prime or not.
+#include <stdio.h>
+
+int main()
+{
+    int n, i, count = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if(count == 2)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
